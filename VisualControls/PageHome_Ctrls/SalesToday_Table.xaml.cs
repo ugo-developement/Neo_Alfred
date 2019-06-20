9aea7b201ff12c3c30cc0f177ae15b59b8fcce17
Joey Hendrich <jlh1100@yahoo.com>
2019-06-20 16:26:00 -0500

    formatted home page sales table

---
+++ VisualControls/PageHome_Ctrls/SalesToday_Table.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Neo_Alfred.VisualControls.PageHome_Ctrls
+{
+    /// <summary>
+    /// Interaction logic for SalesToday_Table.xaml
+    /// </summary>
+    public partial class SalesToday_Table : UserControl
+    {
+        public SalesToday_Table()
+        {
+            InitializeComponent();
+        }
+    }
+}
